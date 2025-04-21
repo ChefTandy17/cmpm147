5a6beb194f5609a3a82c333a655dcce2e3fd7bef
ChefTandy17 <ttandy@ucsc.edu>
2025-04-20 22:49:38 -0700

    updated the date and description

--- experiment3 copy/js/sketch.js
+++ experiment3 copy/js/sketch.js
@@ -1,6 +1,6 @@
-// sketch.js - Creates a generative world
+// sketch.js - Creates a generative dungeon
 // Author: Tyvin Tandy
-// Date:
+// Date: 4/20/25
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
